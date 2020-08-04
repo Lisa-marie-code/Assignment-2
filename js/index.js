6417f46f262470236d47de1427ca24eb2de3d43a
Lisa-marie-code <lisamariekoomson340@gmail.com>
2020-08-04 15:26:44 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+ function getDate(){
+    mainBody.appendChild(para);
+ }
+    const para = document.createElement("p");
+    para.textContent = Date();
+
+    const mainBody = document.getElementById("mainBody")
+    
+
+
